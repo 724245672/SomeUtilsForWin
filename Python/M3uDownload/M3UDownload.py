2b12af24fb46b098ad54019543955209990a7057
kk <724245672@qq.com>
2025-02-21 20:55:31 +0800

    m3u下载工具优化 Signed-off-by: kk <724245672@qq.com>

--- Python/M3uDownload/M3UDownload.py
+++ Python/M3uDownload/M3UDownload.py
@@ -1,4 +1,7 @@
 import os
+import queue
+import re
+import threading
 import tkinter as tk
 from tkinter import messagebox, filedialog
 import subprocess
@@ -19,7 +22,11 @@ class M3U8Downloader:
         self.check_exe()
 
         self.url_entry = None
-        self.savename_entry = None
+        self.save_name_entry = None
+
+        self.process = None
+
+        self.stop_flag = False
 
         # 创建UI元素
         self.create_widgets()
@@ -42,8 +49,14 @@ class M3U8Downloader:
             if not self.exe_path:
                 messagebox.showerror("错误", "未选择 N_m3u8DL-RE.exe，程序将退出。")
                 self.root.quit()
-        else:
-            messagebox.showinfo("信息", f"已找到 N_m3u8DL-RE.exe: {self.exe_path}")
+
+    def display_m3u_content(self):
+        """刷新显示 m3u.txt 内容"""
+        self.m3u_text.delete(1.0, tk.END)
+        if os.path.exists("m3u.txt"):
+            with open("m3u.txt", "r") as m3u_file:
+                content = m3u_file.read()
+                self.m3u_text.insert(tk.END, content)
 
     def create_widgets(self):
         self.root.grid_columnconfigure(0, minsize=12)
@@ -74,8 +87,8 @@ class M3U8Downloader:
         self.url_entry.grid(row=2, column=1, padx=10, pady=5, sticky="ew")
 
         tk.Label(self.root, text="保存文件名:", anchor="w", width=12).grid(row=3, column=0, padx=10, pady=5, sticky="w")
-        self.savename_entry = tk.Entry(self.root)
-        self.savename_entry.grid(row=3, column=1, padx=10, pady=5, sticky="ew")
+        self.save_name_entry = tk.Entry(self.root)
+        self.save_name_entry.grid(row=3, column=1, padx=10, pady=5, sticky="ew")
 
         # 按钮区
         self.button_frame = tk.Frame(self.root)
@@ -85,6 +98,7 @@ class M3U8Downloader:
         self.button_frame.grid_columnconfigure(0, weight=1)
         self.button_frame.grid_columnconfigure(1, weight=1)
         self.button_frame.grid_columnconfigure(2, weight=1)
+        self.button_frame.grid_columnconfigure(3, weight=1)
 
         button_width = 12
         # 左对齐的 "下载" 按钮
@@ -99,7 +113,11 @@ class M3U8Downloader:
 
         # 右对齐的 "保存" 按钮
         self.save_button_main = tk.Button(self.button_frame, text="保存", command=self.save_command, width=button_width)
-        self.save_button_main.grid(row=0, column=2, sticky="e", padx=5)
+        self.save_button_main.grid(row=0, column=2, sticky="", padx=5)
+
+        # 添加 "停止" 按钮
+        self.stop_button = tk.Button(self.button_frame, text="停止下载", command=self.stop_command, width=button_width)
+        self.stop_button.grid(row=0, column=3, sticky="e", padx=5)
 
         # 创建带有滚动条的 Frame 来显示 m3u.txt 内容
         m3u_frame = tk.Frame(self.root)
@@ -122,15 +140,15 @@ class M3U8Downloader:
 
         self.display_m3u_content()
 
-    def display_m3u_content(self):
-        """刷新显示 m3u.txt 内容"""
-        self.m3u_text.delete(1.0, tk.END)
-        if os.path.exists("m3u.txt"):
-            with open("m3u.txt", "r") as m3u_file:
-                content = m3u_file.read()
-                self.m3u_text.insert(tk.END, content)
-        else:
-            self.m3u_text.insert(tk.END, "m3u.txt 文件不存在")
+    def stop_command(self):
+        self.stop_flag = True
+        """停止正在执行的命令"""
+        if self.process:
+            self.process.terminate()
+            self.process = None
+            self.download_log.insert(tk.END, "\n命令已被停止\n")
+            self.download_log.yview(tk.END)
+            self.run_command(f"taskkill /F /IM \"N_m3u8DL-RE.exe\"")
 
     def on_resize(self, event):
         """窗口调整大小时刷新内容"""
@@ -156,60 +174,143 @@ class M3U8Downloader:
             self.save_entry.delete(0, tk.END)
             self.save_entry.insert(0, self.save_dir)
 
-    def download_single(self):
-        """单独下载当前输入的 URL 和文件名"""
-        url = self.url_entry.get().strip()
-        savename = self.savename_entry.get().strip()
+    def get_command(self):
 
-        if not url or not savename:
+        url = self.url_entry.get().strip()
+        save_name = self.save_name_entry.get().strip()
+
+        if not url or not save_name:
             messagebox.showerror("错误", "请输入 URL 和保存文件名。")
             return
 
-        command = f"N_m3u8DL-RE.exe \"{url}\" --save-name \"{savename}\" --check-segments-count false --no-log --tmp-dir \"{self.tmp_dir}\" --save-dir \"{self.save_dir}\""
-        self.run_command(command)
+        if not bool(re.match(r'^(https?|ftp)://[^\s/$.?#].[^\s]*\.m3u8$', url, re.IGNORECASE)):
+            messagebox.showerror("错误", "请输入正确的 URL 。")
+            return
+
+        if os.path.exists("m3u.txt"):
+            with open("m3u.txt", "r", encoding='gbk') as m3u_txt:
+                if url in m3u_txt.read():
+                    messagebox.showerror("错误", "输入的URL已存在 。")
+                    return
+
+        return f"\"{self.exe_path}\" \"{url}\" --save-name \"{save_name}\" --check-segments-count false --no-log --tmp-dir \"{self.tmp_dir}\" --save-dir \"{self.save_dir}\""
+
+    def download_single(self):
+
+        self.stop_flag = False
+
+        command = self.get_command()
+
+        if not command:
+            return
+
+        self.run_command(command)  # 执行命令并获取线程
 
         self.url_entry.delete(0, tk.END)
-        self.savename_entry.delete(0, tk.END)
-        self.display_m3u_content()
+        self.save_name_entry.delete(0, tk.END)
 
-    def run_command(self, command):
+    def run_command(self, command, callback=None):
         """运行命令并显示输出"""
-        self.download_log.delete(1.0, tk.END)
-        process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        for line in process.stdout:
-            self.download_log.insert(tk.END, line.decode("utf-8"))
-        for line in process.stderr:
-            self.download_log.insert(tk.END, line.decode("utf-8"))
-        process.communicate()
+        self.download_log.delete(1.0, tk.END)  # 清空之前的日志
+
+        # 用于接收线程的输出结果
+        result_queue = queue.Queue()
+
+        def execute():
+            try:
+                # 使用 Popen 运行命令
+                self.process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+                # 读取并显示标准输出
+                for line in self.process.stdout:
+                    result_queue.put(line.decode("gbk"))
+
+                # 读取并显示标准错误
+                for line in self.process.stderr:
+                    result_queue.put(line.decode("gbk"))
+
+                # 等待命令执行完成
+                self.process.communicate()
+
+                # 返回执行状态
+                return_code = self.process.returncode
+                if return_code != 0:
+                    result_queue.put(f"命令执行失败，返回码: {return_code}")
+                else:
+                    result_queue.put("命令执行成功")
+            except OSError as e:
+                result_queue.put(f"命令执行失败: {str(e)}")
+            except Exception as e:
+                result_queue.put(f"发生错误: {str(e)}")
+            finally:
+                # 在线程结束时调用回调
+                if callback:
+                    self.root.after(0, callback,None)
+
+        thread = threading.Thread(target=execute, daemon=True)
+        thread.start()
+
+        # 定期检查队列并更新 UI
+        self.download_log.after(100, self.update_log, result_queue)
+
+    def update_log(self, result_queue):
+        """定期检查线程结果并更新UI"""
+        try:
+            while True:
+                output = result_queue.get_nowait()  # 非阻塞式获取
+                self.download_log.insert(tk.END, output)
+                self.download_log.yview(tk.END)  # 滚动到最新行
+        except queue.Empty:
+            pass  # 如果队列空，直接返回
+
+        # 继续定时检查队列
+        self.download_log.after(100, self.update_log, result_queue)
 
     def download_all(self):
+
+        self.stop_flag = False
+
         """下载 m3u.txt 中列出的所有链接"""
         if not os.path.exists("m3u.txt"):
             messagebox.showerror("错误", "m3u.txt 文件不存在。")
             return
 
         with open("m3u.txt", "r") as m3u_file:
-            urls = m3u_file.readlines()
+            command = m3u_file.readline()
+            self.run_command(command,self.after_command_finished)
 
-        for url in urls:
-            url = url.strip()
-            if url:
-                savename = url.split("/")[-1]
-                self.url_entry.delete(0, tk.END)
-                self.url_entry.insert(0, url)
-                self.savename_entry.delete(0, tk.END)
-                self.savename_entry.insert(0, savename)
-                self.download_single()
+    def after_command_finished(self):
+
+        if self.stop_flag:
+            return
+
+        """命令完成后更新 UI 并运行下一个任务"""
+        with open("m3u.txt", "r") as m3u_file:
+            commands = m3u_file.readlines()
+            if len(commands) >= 2:
+                self.display_m3u_content()
+                self.run_command(commands[1], self.after_command_finished)
+        with open("m3u.txt", "w") as m3u_file:
+            if len(commands[1:]) != 0:
+                m3u_file.writelines(commands[1:])
 
     def save_command(self):
-        """保存当前设置"""
-        with open("config.txt", "w") as config_file:
-            config_file.write(f"tmp_dir={self.tmp_dir}\n")
-            config_file.write(f"save_dir={self.save_dir}\n")
-            config_file.write(f"exe_path={self.exe_path}\n")
-        messagebox.showinfo("信息", "设置已保存！")
 
+        command = self.get_command()
 
-if __name__ == "__main__":
+        if not command:
+            return
+
+        with open("m3u.txt", "a+", encoding='gbk') as m3u_txt:
+            m3u_txt.write(command+"\r")
+
+        self.url_entry.delete(0, tk.END)
+        self.save_name_entry.delete(0, tk.END)
+        self.display_m3u_content()
+
+def main():
     app = M3U8Downloader()
     app.root.mainloop()
+
+if __name__ == "__main__":
+    main()
